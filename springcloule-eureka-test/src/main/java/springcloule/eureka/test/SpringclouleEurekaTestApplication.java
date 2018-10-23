409482a7eae0578757ac6d6fbc7522de29db7fc5
Lucifer1993 <18811458650@163.com>
2018-10-23 22:40:47 +0800

    eureka创建

--- springcloule-eureka-test/src/main/java/springcloule/eureka/test/SpringclouleEurekaTestApplication.java
+++ springcloule-eureka-test/src/main/java/springcloule/eureka/test/SpringclouleEurekaTestApplication.java
@@ -2,8 +2,13 @@ package springcloule.eureka.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 使用Eureka做服务发现
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringclouleEurekaTestApplication {
 
 	public static void main(String[] args) {
