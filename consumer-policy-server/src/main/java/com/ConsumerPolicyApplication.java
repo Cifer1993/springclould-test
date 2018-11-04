409079a2af7e5c71bf4d09aeb0038c95dc4320dd
Lucifer1993 <18811458650@163.com>
2018-11-04 21:56:21 +0800

    添加consumer-policy-server服务,并使用rebion进行请求

---
+++ consumer-policy-server/src/main/java/com/ConsumerPolicyApplication.java
@@ -0,0 +1,23 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerPolicyApplication {
+
+	@Bean
+	@LoadBalanced //ribbon的负载均衡注解
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerPolicyApplication.class, args);
+	}
+}
--- provider-user-server/src/main/java/com/controller/UserController.java
+++ provider-user-server/src/main/java/com/controller/UserController.java
@@ -8,25 +8,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.entity.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.serviceregistry.Registration;
 
 /**
  * Created by Administrator on 2018/10/29.
  */
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private Registration registration;
 
-    @RequestMapping("/getById")
-    public User findById(Long id) {
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
         User user = userService.getUserById(id);
         return user;
     }
 
-    @RequestMapping("/say")
+    @GetMapping("/say")
     public void sayHello(){
         System.out.println("================================"+"1111111111"+"=========================");
+    }
+
+    @GetMapping("/getIpAndPort")
+    public String findById() {
+        return registration.getHost() + ":" + registration.getPort();
     }
 }
