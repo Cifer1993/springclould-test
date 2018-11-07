31dfe07dd1730bb43d5f4f76ea9a94e44e4a59ae
Lucifer1993 <18811458650@163.com>
2018-11-07 23:13:35 +0800

    通过fegin进行调用

---
+++ consumer-car-server/src/main/java/com/controller/CarController.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import com.entity.User;
+import com.feign.CarFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2018/11/6.
+ */
+@RestController
+public class CarController {
+    @Autowired
+    private CarFeignClient carFeignClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+
+        return carFeignClient.findById(id);
+    }
+}
