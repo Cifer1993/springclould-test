5244b6669adab88d2db0c3f7c53e870611222639
Lucifer1993 <18811458650@163.com>
2019-01-06 20:07:49 +0800

    接收user-server通过kafka发送的消息

---
+++ message-server/src/main/java/com/componet/MsgConsumer.java
@@ -0,0 +1,17 @@
+package com.componet;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2019/1/6.
+ */
+@Component
+public class MsgConsumer {
+
+    @KafkaListener(topics = {"kafkatest"})
+    public void listenmessage(ConsumerRecord<?, ?> cr){
+        System.out.println("listenT1收到消息,topic:>>>" + cr.topic() + "  offset:>>" + cr.offset()+ "  key:>>" + cr.key() + "  value:>>" + cr.value());
+    }
+}
